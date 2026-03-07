ef03ff219f08a55d5f308eaa55ff6ac7746b6659
alexcibotari <alexandru.cibotari@gmail.com>
2026-03-07 16:49:09 +0100

    Update References

--- packages/react/src/localess-componenet.tsx
+++ packages/react/src/localess-componenet.tsx
@@ -1,14 +1,15 @@
 import {forwardRef} from "react";
-import {ContentData, Links, localessEditable} from "@localess/client";
+import {ContentData, Links, localessEditable, References} from "@localess/client";
 import {FONT_BOLD, FONT_NORMAL} from "./console";
 import {getComponent, getFallbackComponent, isSyncEnabled} from "./index";
 
 export type LocalessComponentProps<T extends ContentData = ContentData> = {
   data: T
   links?: Links
+  references?: References;
 }
 
-export const LocalessComponent = forwardRef<HTMLElement, LocalessComponentProps>(({data, links, ...restProps}, ref) => {
+export const LocalessComponent = forwardRef<HTMLElement, LocalessComponentProps>(({data, links, references, ...restProps}, ref) => {
   if (!data) {
     console.error('LocalessComponent property %cdata%c is not provided.', FONT_BOLD, FONT_NORMAL)
     return <div>LocalessComponent property <b>data</b> is not provided.</div>
@@ -17,12 +18,12 @@ export const LocalessComponent = forwardRef<HTMLElement, LocalessComponentProps>
   const Comp = getComponent(data._schema || data.schema);
   if (Comp) {
     const attr = isSyncEnabled() ? localessEditable(data) : {};
-    return <Comp ref={ref} data={data} links={links} {...attr} {...restProps} />;
+    return <Comp ref={ref} data={data} links={links} references={references} {...attr} {...restProps} />;
   }
   // Try to use Fallback Component
   const FallbackComponent = getFallbackComponent()
   if (FallbackComponent) {
-    return <FallbackComponent ref={ref} data={data} links={links} {...restProps} />
+    return <FallbackComponent ref={ref} data={data} links={links} references={references} {...restProps} />
   }
   // Missing Configuration case
   return (
