3324b96897e3b1f019b0a76083910ba836374284
alexcibotari <alexandru.cibotari@gmail.com>
2026-04-25 22:39:13 +0200

    Remove debug flag from session initialization

--- packages/cli/src/commands/types/generate/index.ts
+++ packages/cli/src/commands/types/generate/index.ts
@@ -28,7 +28,6 @@ export const typesGenerateCommand = new Command('generate')
       origin: session.origin,
       spaceId: session.space,
       token: session.token,
-      debug: true,
     });
 
     console.log('Fetching schemas from Localess...');
