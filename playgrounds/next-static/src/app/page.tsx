5c88aae88b19d794f45317c11775db8fe76f71b8
alexcibotari <alexandru.cibotari@gmail.com>
2026-08-07 19:43:28 +0200

    feat: remove version field from localessInit configuration in page.tsx

--- playgrounds/next-static/src/app/page.tsx
+++ playgrounds/next-static/src/app/page.tsx
@@ -6,7 +6,6 @@ import {PageLocaless} from "@/shared/components/localess/page";
 localessInit({
   origin: "https://demo.localess.org", // Replace it for your origin
   spaceId: "MmaT4DL0kJ6nXIILUcQF", // Replace it for your spaceId
-  version: "draft",
   token: "Y4rvboPnyzVeC7LddEK5", // Replace it for your token
   debug: true,
   components: {
